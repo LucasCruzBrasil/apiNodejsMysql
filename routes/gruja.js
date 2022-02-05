dedc2749eedeed977dfd74b13263ee73b0d470dc
Lucas <lucasAngular@hotmail.com>
2022-02-05 05:56:40 -0300

    ajuste no endpoint busca por id

--- routes/gruja.js
+++ routes/gruja.js
@@ -8,7 +8,7 @@ router.post('/', grujaControllers.postGruja); // insere uma gorjeta
 router.get('/', grujaControllers.getGruja); //lista gorjetas
 router.get('/:nome', grujaControllers.getNomeGruja); //lista gorjetas por nome
 
-router.get('/:id_gruja', grujaControllers.getUmaGrujaEspecifica);//lista por id específico
+router.get('/v/:id_gruja', grujaControllers.getUmaGrujaEspecifica);//lista por id específico
 router.get('/v/:nome', grujaControllers.getValorByName)//lista total valor de uma pessoa
 router.patch('/', grujaControllers.getAtualizaUmaGorjeta); //altera gorjeta 
 router.delete('/:id_gruja', grujaControllers.getDeletaUmaGorjeta); //deleta uma gorjeta 
