ebddfed88a7a8b611b22afea87aacc47a84d39a7
Lucas <lucasAngular@hotmail.com>
2022-02-11 01:49:31 -0300

    finalizando json do insere valores

--- controllers/valores-controllers.js
+++ controllers/valores-controllers.js
@@ -1,27 +1,40 @@
 const { response, query } = require('express');
 const mysql = require('../mysql');
 
-exports.postInsereValores = async (req,res, next) => {
+exports.postInsereValores = async (req, res, next) => {
     try {
         const query = "INSERT INTO valores (valor_cartao, valor_dinheiro, valor_pix, valor_pic_pay, data_valor, qtd_pessoas ) VALUES (?,?,?,?,?,?)";
-        const result = mysql.execute(query, [
+        const result = await mysql.execute(query, [
             req.body.valor_cartao,
             req.body.valor_dinheiro,
             req.body.valor_pix,
             req.body.valor_pic_pay,
             req.body.data_valor,
-            req.body.qtd_pessoas
+            req.body.qtd_pessoas,
+
         ]);
+        let soma =
+            req.body.valor_cartao
+            + req.body.valor_dinheiro
+            + req.body.valor_pix
+            + req.body.valor_pic_pay;
+
+            let divisao = soma / req.body.qtd_pessoas;
+
 
         const response = {
+
             mensagem: "Valor do dia inserido com sucesso",
             id_valor: result.id_valor,
             valor_cartao: req.body.valor_cartao,
             valor_dinheiro: req.body.valor_dinheiro,
             valor_pix: req.body.valor_pix,
-            valor_pic_pay:req.body.valor_pic_pay,
-            dia:req.body.data_valor,
-            quantidade_de_pessoas:req.body.qtd_pessoas,
+            valor_pic_pay: req.body.valor_pic_pay,
+            dia: req.body.data_valor,
+            quantidade_de_pessoas: req.body.qtd_pessoas,
+            valor_Total: soma,
+            valor_por_pessoa: divisao,
+            
             request: {
                 tipo: 'POST',
                 descricao: "Valor do dia",
